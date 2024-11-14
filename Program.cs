a39a317470952fb0438d55ffee78ef6b4b316bcb
carobte <caro.bustamante.escobar@gmail.com>
2024-11-14 06:56:59 -0500

    feat: welcome page

--- Program.cs
+++ Program.cs
@@ -23,8 +23,6 @@ var conectionDB = $"server={dbHost};port={dbPort};database={dbName};uid={dbUser}
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(conectionDB, ServerVersion.Parse("8.0.15-mysql")));
 
-
-
 // Add services to the container.
 
 builder.Services.AddSingleton<PasswordHasher>();
@@ -67,6 +65,33 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+// Basic welcome page with navigation to Swagger page 
+
+app.Use(async (context, next) =>
+{
+    if (context.Request.Path == "/")
+    {
+        var htmlContent = @"
+        <html>
+            <head>
+                <title>CordiSimple API</title>
+            </head>
+            <body style='font-family: Arial, sans-serif; background-color: #f4f4f4; text-align: center; height: 90%; display: flex; flex-direction: column; justify-content: center; align-items: center;'>
+                <h1 style='color: #333; font-size: 36px;'>Welcome to CordiSimple API</h1>
+                <a href='/swagger' style='color: #007bff; text-decoration: none;'> Click here to Swagger documentation</a>
+            </body>
+        </html>";
+
+        context.Response.ContentType = "text/html";
+        await context.Response.WriteAsync(htmlContent);
+    }
+    else
+    {
+        await next();
+    }
+});
+
 
 app.MapControllers();
 
